6fe55330279a2296c18a9ae102ed9aef4eef1d68
Junior Ogun <olajideogun123@gmail.com>
2020-08-30 22:55:49 -0400

    assignment 1 solution

--- solution.py
+++ solution.py
@@ -1,29 +1,48 @@
-a_file = open('problem1/input-1.txt', "r")
-
+import sys 
+a_file = open(sys.argv[1], "r")
 list_of_lists = []
 for line in a_file:
   stripped_line = line.strip()
   line_list = stripped_line.split()
   line_list = list(map(int, line_list))
   list_of_lists.append(line_list)
-
 a_file.close()
 
 length1  = list_of_lists[0][0]
 length2 = list_of_lists[1][0]
-print(length1,length2)
 
 
 
 def sort(list1,list2):
+  new_list = []
   i = 0
   j = 0
+  k = 0
   if length1 == 0:
-    print(list2)
+    return([length2] + list2)
   if length2 == 0:
-    print(list1)
-  while i < length1 and j < length2:
-    print(i,j)
+    return([length1] + list1)
 
-sort(list_of_lists[0][1:],list_of_lists[1][1:])
+
+
+  while i < length1 and j < length2:
+    if list1[i] < list2[j]:
+      new_list.append(list1[i])
+      i = i + 1
+    elif list1[i] > list2[j]:
+      new_list.append(list2[j])
+      j = j + 1
+
+    if j == length2:
+      new_list = new_list + list1[i:]
+    if i == length2:
+      new_list = new_list + list2[j:]
+  
+  return ([length1 + length2]+ new_list)
+
+
+result = sort(list_of_lists[0][1:],list_of_lists[1][1:])
+outF = open(sys.argv[2], "w")
+outF.write(str(result))
+outF.close()
   
